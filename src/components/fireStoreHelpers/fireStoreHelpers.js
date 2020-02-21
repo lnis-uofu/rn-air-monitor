1772deacbb206a7c3e19ffb31bc17b4f0c059e7d
Quang Nguyen <ntdquang1412@gmail.com>
2020-02-20 22:50:31 -0700

    Added filestore helper to put new device to firebase database

--- src/components/fireStoreHelpers/fireStoreHelpers.js
+++ src/components/fireStoreHelpers/fireStoreHelpers.js
@@ -1,5 +1,6 @@
 import FireStoreConstants from '../Constants/fireStoreConstants';
 import firebase from 'react-native-firebase';
+const usersCollection = 'users';
 
 export default class FireStoreHelper {
   static async getUserDevices() {
@@ -22,4 +23,66 @@ export default class FireStoreHelper {
       });
     return await Promise.resolve(devicesData);
   }
+
+  static addDeviceInfoToFireBaseDataBase = async (macAddress, label) => {
+    console.log('Get devices info!');
+    var isDuplicated = false;
+    var dev_map = {mac_add: macAddress, user_label: label};
+    const refUser = await firebase
+      .firestore()
+      .collection(usersCollection)
+      .doc(global.email);
+
+    console.log('firestoreHelpers.addDeviceInfoToFireBaseDataBase');
+    await firebase
+      .firestore()
+      .runTransaction(async transaction => {
+        const doc = await transaction.get(refUser);
+
+        // if it does not exist set the entry
+        if (!doc.exists) {
+          console.log('Not EXISTED');
+          const newDeviceData = new Array();
+          newDeviceData.push(macAddress);
+          transaction.set(refUser, {devices: newDeviceData});
+          return 1;
+        }
+        console.log('EXISTED');
+        const devicesData = doc.data().devices;
+        console.log(devicesData);
+        // Check to see if there is such field
+        if (devicesData) {
+          // Check duplication
+          devicesData.forEach(device => {
+            console.log(device.mac_add);
+            console.log(macAddress);
+            if (macAddress.includes(device.mac_add)) {
+              console.log('Found duplication');
+              isDuplicated = true;
+            }
+          });
+          if (isDuplicated !== true) {
+            // devicesData.push(macAddress);
+            devicesData.push(dev_map);
+            console.log('New Devices data ');
+            console.log(devicesData);
+            // Update new device information to the current table
+            transaction.update(refUser, {
+              devices: devicesData,
+            });
+          } else {
+            console.log('Found duplication');
+          }
+        } else {
+          const newDeviceData = new Array();
+          newDeviceData.push(dev_map);
+          // Update new information to database
+          transaction.update(refUser, {devices: newDeviceData});
+          return 1;
+        }
+      })
+      .catch(err => {
+        console.warn('addDeviceInfoToFireBaseDataBase' + err);
+      });
+  };
 }
